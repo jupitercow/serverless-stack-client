330225112d102f225c8ef961c502881d8aab5d3a
Jacob Snyder <jake@tracflo.io>
2020-06-13 15:09:05 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "hoa-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://ldxxglo491.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_7OSfzLDi1",
+    APP_CLIENT_ID: "1ulavtnp8asp4h3u9ljhka5q3r",
+    IDENTITY_POOL_ID: "us-east-1:fa3c844d-e073-4638-9075-7708b9ceadb4"
+  }
+};
